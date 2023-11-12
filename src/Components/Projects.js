5482449f56ebc8125f1c94040a82b5359d22d737
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-12 22:07:56 +0300

    del class

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,13 +4,13 @@ import ProjectsArray from "../Utils/ProjectsArray";
 
 const Profile = () => {
   return (
-    <div className="text-center">
+
     <section className="section-project">
     <h1 className="section-profile-header">A Few of My Projects</h1>
       <ProjectsArray />
      
     </section>
-    </div>
+
   );
 };
 export default Profile;
